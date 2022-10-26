61f905c9347a53f4e154867b76f6eac1b44361c5
maanavnair <114719385+maanavnair@users.noreply.github.com>
2022-10-26 19:09:44 +0530

    PATTERN
    A c++ program to print a triangular pattern

--- pattern.cpp
+++ pattern.cpp
@@ -1,20 +1,15 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main(){
-
+    cout<<"Enter the number of rows"<<endl;
     int n;
     cin>>n;
-
-    int i=1;
-    while(i<=n){
-        int j=1;
-        while(j<=n){
-            cout<<"*";
-            j=j+1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
         }
         cout<<endl;
-        i=i+1;        
     }
-return 0;
+    return 0;
 }
